615bf75870c46371a286ee2ccbfababbde482b9b
Alex Ryan <thomas.alexander.ryan@gmail.com>
2015-01-26 08:48:44 -0500

    Initial Commit

---
+++ EVNTR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EVNTR
+//
+//  Created by Alex Ryan on 1/26/15.
+//  Copyright (c) 2015 U2PrideLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EVNTR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EVNTR
+//
+//  Created by Alex Ryan on 1/26/15.
+//  Copyright (c) 2015 U2PrideLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
